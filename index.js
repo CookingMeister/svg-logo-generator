11162738f99fbbfa7cb4e19cccaca28bc7f4b222
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-01-05 14:13:06 -0400

    superclass Shape, subclasses square, circle, triangle created and exported,  switch function for shape selected created with render methos embedded

--- index.js
+++ index.js
@@ -1,43 +1,78 @@
-import inquirer from 'inquirer';
-import validator from 'validator';
-import fs from 'fs';
-import { generateCircleSVG, generateTriangleSVG, generateSquareSVG } from './lib/shapes.js';
+import inquirer from "inquirer";
+import validator from "validator";
+import fs from "fs";
+import { Shape, Circle, Triangle, Square } from "./lib/shapes.js";
 
 // Prompt user for input
+async function promptUser() {
   const text = await inquirer.prompt([
     {
-      type: 'input',
-      name: 'text',
-      message: 'What are your three characters for the text?'
-    }
+      type: "input",
+      name: "text",
+      message: "What are your three characters for the text?",
+    },
   ]);
 
   const textColor = await inquirer.prompt([
     {
-      type: 'input',
-      name: 'textColor',
-      message: 'What is the color of the text?'
-    }
+      type: "input",
+      name: "textColor",
+      message: "What is the color of the text?",
+    },
   ]);
 
   const shape = await inquirer.prompt([
     {
-      type: 'list',
-      name: 'shape',
-      message: 'What shape would you like to generate?',
-      choices: ['Circle', 'Triangle', 'Square']
-    }
-  ]);
-  
-  const fillColor = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'fillColor',
-      message: 'What is the fill color of the shape?'
-    }
+      type: "list",
+      name: "shape",
+      message: "What shape would you like to generate?",
+      choices: ["Circle", "Triangle", "Square"],
+    },
   ]);
 
-  // Save SVG to file
-const logo = `${shape.shape}.svg`;
+  const fillColor = await inquirer.prompt([
+    {
+      type: "input",
+      name: "fillColor",
+      message: "What is the fill color of the shape?",
+    },
+  ]);
+}
+
+// Generate SVG
+const answers = await promptUser();
+const logo = generateLogo(
+  answers.text,
+  answers.textColor,
+  answers.shape,
+  answers.fillColor
+);
+function generateLogo() {
+  switch (answers.shape) {
+    case "Circle":
+      svg = new Circle(
+        answers.fillColor,
+        answers.textColor,
+        answers.text
+      ).render();
+      break;
+    case "Triangle":
+      svg = new Triangle(
+        answers.fillColor,
+        answers.textColor,
+        answers.text
+      ).render();
+      break;
+    case "Square":
+      svg = new Square(
+        answers.fillColor,
+        answers.textColor,
+        answers.text
+      ).render();
+      break;
+  }
+}
+
+// Save SVG to file
 fs.writeFileSync(logo, svg);
 console.log(`Generated ${logo}`);
