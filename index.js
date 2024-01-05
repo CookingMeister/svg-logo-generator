9261d35dd2c3016abf582891ab5a707485a8cc44
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-01-05 15:14:30 -0400

    fixed import/export issue, functions changed to async, try, await format, validator package integrated into each prompt

--- index.js
+++ index.js
@@ -5,74 +5,89 @@ import { Shape, Circle, Triangle, Square } from "./lib/shapes.js";
 
 // Prompt user for input
 async function promptUser() {
-  const text = await inquirer.prompt([
-    {
-      type: "input",
-      name: "text",
-      message: "What are your three characters for the text?",
-    },
-  ]);
-
-  const textColor = await inquirer.prompt([
-    {
-      type: "input",
-      name: "textColor",
-      message: "What is the color of the text?",
-    },
-  ]);
-
-  const shape = await inquirer.prompt([
-    {
-      type: "list",
-      name: "shape",
-      message: "What shape would you like to generate?",
-      choices: ["Circle", "Triangle", "Square"],
-    },
-  ]);
-
-  const fillColor = await inquirer.prompt([
-    {
-      type: "input",
-      name: "fillColor",
-      message: "What is the fill color of the shape?",
-    },
-  ]);
-}
-
-// Generate SVG
-const answers = await promptUser();
-const logo = generateLogo(
-  answers.text,
-  answers.textColor,
-  answers.shape,
-  answers.fillColor
-);
-function generateLogo() {
-  switch (answers.shape) {
-    case "Circle":
-      svg = new Circle(
-        answers.fillColor,
-        answers.textColor,
-        answers.text
-      ).render();
-      break;
-    case "Triangle":
-      svg = new Triangle(
-        answers.fillColor,
-        answers.textColor,
-        answers.text
-      ).render();
-      break;
-    case "Square":
-      svg = new Square(
-        answers.fillColor,
-        answers.textColor,
-        answers.text
-      ).render();
-      break;
+  try {
+    const text = await inquirer.prompt([
+      {
+        type: "input",
+        name: "text",
+        message: "What are your three characters for the text?",
+        validate: (input) => {
+          if (validator.isEmpty(input)) {
+            return "Text cannot be empty";
+          }
+          return true;
+        },
+      },
+    ]);
+    const textColor = await inquirer.prompt([
+      {
+        type: "input",
+        name: "textColor",
+        message: "What is the color of the text?",
+        validate: (input) => {
+          if (validator.isEmpty(input)) {
+            return "Text color cannot be empty";
+          }
+          return true;
+        },
+      },
+    ]);
+    const shape = await inquirer.prompt([
+      {
+        type: "list",
+        name: "shape",
+        message: "What shape would you like to generate?",
+        choices: ["Circle", "Triangle", "Square"],
+      },
+    ]);
+    const fillColor = await inquirer.prompt([
+      {
+        type: "input",
+        name: "fillColor",
+        message: "What is the fill color of the shape?",
+        validate: (input) => {
+          if (validator.isEmpty(input)) {
+            return "Shape color cannot be empty";
+          }
+          return true;
+        },
+      },
+    ]);
+    return text, textColor, shape, fillColor;
+  } catch (err) {
+    console.log(err);
+    throw err;
   }
 }
 
-// Save SVG to file
-fs.writeFileSync(logo, svg);
-console.log(`Generated ${logo}`);
+// Generate SVG
+const init = async () => {
+  try {
+    const logo = async () => {
+      await promptUser();
+      return generateLogo(text, textColor, shape, fillColor);
+    };
+    // generateLogo(logo);
+    // Save SVG to file
+    await fs.promises.writeFile(logo, svg);
+    console.log(`Generated ${logo}`);
+  } catch {
+    console.log(err);
+    throw err;
+  }
+};
+
+function generateLogo(text, textColor, shape, fillColor) {
+  switch (shape) {
+    case "Circle":
+      svg = new Circle(fillColor, textColor, text).render();
+      break;
+    case "Triangle":
+      svg = new Triangle(fillColor, textColor, text).render();
+      break;
+    case "Square":
+      svg = new Square(fillColor, textColor, text).render();
+      break;
+  }
+}
+init();
